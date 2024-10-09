8ac1efb78c58b7b196488a301840039ee909f22f
San7o <santigio2003@gmail.com>
2024-10-09 15:30:35 +0200

    project: setup

---
+++ README.md
@@ -0,0 +1,42 @@
+# ssap - simply secure all passwords
+
+ssap is a secure and **blazingly fast** password manager tool written in **Rust**.
+You can safely store, organize, generate and retrieve passwords from your
+local hard disk **blazingly quickly**. All the passwords will be stored in an encrypted form
+
+> Note: the project is currently in early developement and is changing **blazingly rapidly**
+
+saap is:
+
+- **secure**: written in Rust, but using battle tested encryption libraries (openssl) written in **C**
+- **blazingly fast**: did I tell you that this program is **blazingly fast**? It is because It's written in Rust!
+- **kiss**: keep It simple, stupendo! ssap follows the unix principle of doing one thing and doing it right.
+
+## Usage
+Compile the program:
+```bash
+cargo build
+```
+Run ssap:
+```bash
+cargo run
+```
+Run tests:
+```bash
+cargo test
+```
+Build documentation:
+```bash
+cargo doc
+```
+Format code:
+```bash
+cargo fmt
+```
+
+## TODO
+- encrypt / decrypt
+- store / read from disk
+- multiple encryption algorithms
+- 2fa?
+- **blazing speed**
---
+++ src/main.rs
@@ -0,0 +1,101 @@
+use openssl::aes::{aes_ige, AesKey, KeyError};
+use openssl::symm::Mode;
+
+struct Ssap {
+    // TODO: Add fields here
+}
+
+fn main() {
+    let key = b"secret".to_vec();
+    let plaintext = b"Ciaone".to_vec();
+    // TODO: generate random first iv
+    let mut iv = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F".to_vec();
+    println!("starting iv: {:?}", iv);
+    println!(
+        "Ciphertext: {:?}",
+        encrypt_aes128(plaintext, key, &mut iv).unwrap()
+    );
+    println!("iv: {:?}", iv);
+}
+
+/// Encrypt an arbitrary number of bytes into a cypher text with the same
+/// length using the provided key for encryption.
+fn encrypt_aes128(
+    mut plaintext: Vec<u8>,
+    mut key: Vec<u8>,
+    iv: &mut [u8],
+) -> Result<Vec<u8>, KeyError> {
+    if plaintext.len() % 16 != 0 {
+        plaintext.resize(plaintext.len() / 16 * 16 + 16, 0 as u8);
+    }
+    if key.len() % 16 != 0 {
+        key.resize(16, 0 as u8);
+    }
+
+    let mut ciphertext: Vec<u8> = Vec::new();
+    for i in (0..plaintext.len()).step_by(16) {
+        let block = &plaintext[i..i + 16];
+        let aes_key_r = AesKey::new_encrypt(key.as_ref())?;
+        let mut out: &mut [u8] = &mut [0; 16];
+        aes_ige(block, &mut out, &aes_key_r, iv, Mode::Encrypt);
+        ciphertext.append(&mut Vec::from(out));
+    }
+
+    return Ok(ciphertext);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encrypt() {
+        let key = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F".to_vec();
+        let plaintext =
+            b"\x12\x34\x56\x78\x90\x12\x34\x56\x12\x34\x56\x78\x90\x12\x34\x56".to_vec();
+        let mut iv = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F".to_vec();
+
+        let output = encrypt_aes128(plaintext, key, &mut iv);
+        assert_eq!(
+            output.unwrap(),
+            b"\xa6\xad\x97\x4d\x5c\xea\x1d\x36\xd2\xf3\x67\x98\x09\x07\xed\x32"
+        );
+    }
+}
+
+fn decrypt_aes128(
+    mut ciphertext: Vec<u8>,
+    mut key: Vec<u8>,
+    start_iv: &mut [u8],
+) -> Result<Vec<u8>, KeyError> {
+    let mut plaintext: Vec<u8> = Vec::new();
+    if ciphertext.len() % 16 != 0 {
+        // TODO Custom error
+    }
+    if key.len() % 16 != 0 {
+        key.resize(16, 0 as u8);
+    }
+
+    let mut ciphertext: Vec<u8> = Vec::new();
+    for i in (0..ciphertext.len()).step_by(16) {
+        let block = &ciphertext[i..i + 16];
+        let aes_key_r = AesKey::new_decrypt(key.as_ref())?;
+        let mut out: &mut [u8] = &mut [0; 16];
+        aes_ige(block, &mut out, &aes_key_r, start_iv, Mode::Decrypt);
+        plaintext.append(&mut Vec::from(out));
+    }
+
+    return Ok(plaintext);
+}
+
+fn help() {
+    // TODO
+}
+
+fn store() {
+    // TODO
+}
+
+fn read_disk() {
+    // TODO
+}
